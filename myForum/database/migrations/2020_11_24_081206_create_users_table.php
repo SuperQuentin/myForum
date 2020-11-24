059fd1a442603cc1a043b7c6275d830d32d1b693
grafique3d <grafique3d@gmail.com>
2020-11-24 09:17:34 +0100

    create users tables migration based on existing db model

---
+++ myForum/database/migrations/2020_11_24_081206_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name','100');
+            $table->string('last_name','100');
+            $table->string('pseudo','10');
+            $table->foreignId('role_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
