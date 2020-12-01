4f5fc4b8ec1da1c2588a4e382048d280b987a301
grafique3d <grafique3d@gmail.com>
2020-12-01 09:13:52 +0100

    add resources routes for ReferenceController

--- myForum/routes/web.php
+++ myForum/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ReferenceController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('homepage');
-});
+Route::get('/', [ReferenceController::class, 'index']);
+Route::get('/', [ReferenceController::class, 'index']);
+Route::resource('/references',ReferenceController::class);
